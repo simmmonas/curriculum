e265ae6252aede320900d9b36e3dbe30378ece07
Simona <simona.semancova@gmail.com>
2018-09-17 17:10:52 +0200

    I'm pushing everything

---
+++ main.js
@@ -0,0 +1,25 @@
+const templateSemesters = document.querySelector("template").content;
+const semTitle = document.querySelector(".semTitle");
+const parent = document.querySelector("main");
+window.addEventListener("load", ()=>{
+    fetch("curriculum1.json").then(res=>res.json()).then(showSemesters)
+
+})
+
+
+
+function showSemesters(data){
+    console.log(data);
+   semTitle.textContent = data.title;
+    data.articles.forEach(showSingleSemester)
+
+
+}
+function showSingleSemester(semester){
+    console.log(semester)
+    const clone = templateSemesters.cloneNode(true);
+    clone.querySelector("h2").textContent=semester.title;
+
+    parent.appendChild(clone);
+}
+
